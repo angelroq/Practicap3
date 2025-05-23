e845bf112ee541c383894c9aca15ea0a457ca8ed
[Xander <[xreyeso@miumg.edu.gt]>
2025-05-23 16:56:39 -0600

    suma

--- practica1/main.cpp
+++ practica1/main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int numero1, numero2;
+    int suma;
+    cout << "ingrese el primer numero: ";
+    cin >> numero1;
+    cout << "ingrese el segundo numero ";
+    cin >> numero2;
+
+    suma = numero1 + numero2;
+
+    cout << "La suma es: " << suma << endl;
     return 0;
 }
